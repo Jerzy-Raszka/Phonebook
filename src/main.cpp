dd69bbc0341bb50a179025b5c9727471883d55bd
Jerzy Raszka <jerzyraszka2000@gmail.com>
2024-11-01 12:56:20 +0100

    Learning std::list and std::find

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include <string>
-
+#include <list>
 #include "../include/ContactData.h"
 
-int main() {
+int main()
+{
+    std::list<ContactData> contacts;
+
     const auto x = ContactData("Jerzy", "Raszka", "669114472");
-    std::cout << x.name;
-    std::cout << x.lastname;
-    std::cout << x.phoneNumber;
+    contacts.push_front(x);
+    const auto y = ContactData("Adam", "Raszka", "669114472");
+    contacts.push_front(y);
+    const auto z = ContactData("Romek", "Raszka", "669114472");
+
+    contacts.emplace_back("Bartek", "Raszka", "669114472");
+
+
+    for (const auto& n : contacts)
+    {
+        std::cout << n.name << std::endl;
+    }
+
+    //How to find pos of element with matching object name
+    if (auto it = std::find(contacts.begin(), contacts.end(), name == "Jerzy"); it != contacts.end())
+    {
+        contacts.insert(it, z);
+    }
+
     return 0;
 }
