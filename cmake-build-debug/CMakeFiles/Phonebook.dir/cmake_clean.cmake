6b95f0e161075ca13e89ee9e0b626eb9eca9df65
Jerzy Raszka <jerzyraszka2000@gmail.com>
2024-11-01 12:56:59 +0100

    Learning std::list and std::find

---
+++ cmake-build-debug/CMakeFiles/Phonebook.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Phonebook.dir/src/ContactData.cpp.obj"
+  "CMakeFiles/Phonebook.dir/src/ContactData.cpp.obj.d"
+  "CMakeFiles/Phonebook.dir/src/main.cpp.obj"
+  "CMakeFiles/Phonebook.dir/src/main.cpp.obj.d"
+  "Phonebook.exe"
+  "Phonebook.exe.manifest"
+  "Phonebook.pdb"
+  "libPhonebook.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Phonebook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
