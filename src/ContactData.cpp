a1eace5c5eeb750ebdef85512acb4c1b5468e325
Jerzy Raszka <jerzyraszka2000@gmail.com>
2024-11-02 19:44:09 +0100

    Added alphabetical order by name in contacts

--- src/ContactData.cpp
+++ src/ContactData.cpp
@@ -9,26 +9,23 @@ std::list<std::shared_ptr<ContactData>> ContactData::contacts;
 
 void addContact(const ContactData& addedContact)
 {
-    if (ContactData::contacts.empty())
+    //TODO: if name is the same check lastname
+    bool wasAdded = false;
+    for (auto it = ContactData::contacts.begin(); it != ContactData::contacts.end(); ++it)
     {
-        ContactData::contacts.push_front(std::make_shared<ContactData>(addedContact));
-    }
-    else
-    {
-        for (auto it = ContactData::contacts.begin(); it != ContactData::contacts.end(); ++it)
+        if (addedContact.name.compare((*it)->name) < 0)
         {
-            if (addedContact.name.compare((*it)->name) < 0)
-            {
-                ContactData::contacts.insert(it, std::make_shared<ContactData>(addedContact));
-                break;
-            }
-            if (it == ContactData::contacts.end()) //Check why this never happens
-            {
-                ContactData::contacts.push_back(std::make_shared<ContactData>(addedContact));
-            }
+            ContactData::contacts.insert(it, std::make_shared<ContactData>(addedContact));
+            wasAdded = true;
+            break;
         }
     }
+    if (!wasAdded)
+    {
+        ContactData::contacts.push_back(std::make_shared<ContactData>(addedContact));
+    }
 }
+
 
 ContactData::ContactData(const std::string& name, const std::string& lastname, const std::string& phoneNumber)
 {
--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,9 @@
 int main()
 {
     ContactData con1("Adam", "Raszka", "669114472");
+    ContactData con2("Bartek", "Raszka", "669114472");
     ContactData con4("Damian", "Raszka", "669114472");
     ContactData con3("Czarek", "Raszka", "669114472");
-    ContactData con2("Bartek", "Raszka", "669114472");
-
 
     for (const auto& contact : ContactData::contacts)
     {
