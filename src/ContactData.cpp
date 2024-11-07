21362fcce4e2cf14158d5cf7f5937f1a43da0d0b
Jerzy Raszka <jerzyraszka2000@gmail.com>
2024-11-07 12:26:11 +0100

    Added text menu with all option to manage phonebook by user

--- src/ContactData.cpp
+++ src/ContactData.cpp
@@ -7,49 +7,41 @@
 #include <fstream>
 #include <iostream>
 
-std::list<std::shared_ptr<ContactData>> ContactData::contacts;
+std::list<std::shared_ptr<ContactData> > ContactData::contacts;
 
-std::string toLowerCase(const std::string& str)
-{
+std::string toLowerCase(const std::string &str) {
     std::string lowerStr = str;
     std::ranges::transform(lowerStr, lowerStr.begin(), ::tolower);
     return lowerStr;
 }
 
-
-
-void addContactAndSort(const ContactData& addedContact)
-{
+void addContactAndSort(const ContactData &addedContact) {
     bool isAdded = false;
     const auto newNameToLower = toLowerCase(addedContact.name);
     const auto newLastNameToLower = toLowerCase(addedContact.lastname);
 
-    for (auto it = ContactData::contacts.begin(); it != ContactData::contacts.end(); ++it)
-    {
+    for (auto it = ContactData::contacts.begin(); it != ContactData::contacts.end(); ++it) {
         auto currentNameToLower = toLowerCase((*it)->name);
         auto currentLastNameToLower = toLowerCase((*it)->lastname);
 
-        if (newNameToLower.compare(currentNameToLower) < 0)
-        {
+        if (newNameToLower.compare(currentNameToLower) < 0) {
             ContactData::contacts.insert(it, std::make_shared<ContactData>(addedContact));
             isAdded = true;
             break;
         }
-        if (newNameToLower == currentNameToLower && newLastNameToLower.compare(currentLastNameToLower) < 0)
-        {
+        if (newNameToLower == currentNameToLower && newLastNameToLower.compare(currentLastNameToLower) < 0) {
             ContactData::contacts.insert(it, std::make_shared<ContactData>(addedContact));
             isAdded = true;
             break;
         }
     }
-    if (!isAdded)
-    {
+    if (!isAdded) {
         ContactData::contacts.push_back(std::make_shared<ContactData>(addedContact));
     }
 }
 
-void ContactData::printList(const std::list<std::shared_ptr<ContactData>>& listToPrint) {
-    for (const auto & it : listToPrint) {
+void ContactData::printList(const std::list<std::shared_ptr<ContactData> > &listToPrint) {
+    for (const auto &it: listToPrint) {
         std::cout << it->name << " ";
         std::cout << it->lastname << " ";
         std::cout << it->phoneNumber << std::endl;
@@ -61,12 +53,12 @@ bool ContactData::operator==(const ContactData &other) const {
     return this->name == other.name && this->lastname == other.lastname && this->phoneNumber == other.phoneNumber;
 }
 
-void ContactData::searchContacts(const std::string& searchParam)
-{
+void ContactData::searchContacts(const std::string &searchParam) {
     auto searchResult = ContactData::contacts;
 
-    for(auto it = searchResult.begin(); it != searchResult.end(); ++it) {
-        if(auto fullData = toLowerCase((*it)->name + (*it)->lastname + (*it)->phoneNumber); fullData.find(searchParam) == std::string::npos) {
+    for (auto it = searchResult.begin(); it != searchResult.end(); ++it) {
+        if (auto fullData = toLowerCase((*it)->name + (*it)->lastname + (*it)->phoneNumber);
+            fullData.find(searchParam) == std::string::npos) {
             it = searchResult.erase(it);
             --it;
         }
@@ -74,8 +66,8 @@ void ContactData::searchContacts(const std::string& searchParam)
     printList(searchResult);
 }
 
-void ContactData::removeContact(const ContactData& contactToRemove) {
-    for(auto it = ContactData::contacts.begin(); it != ContactData::contacts.end(); ++it) {
+void ContactData::removeContact(const ContactData &contactToRemove) {
+    for (auto it = ContactData::contacts.begin(); it != ContactData::contacts.end(); ++it) {
         if (**it == contactToRemove) {
             contacts.erase(it);
             break;
@@ -83,7 +75,7 @@ void ContactData::removeContact(const ContactData& contactToRemove) {
     }
 }
 
-void ContactData::serialize(const std::string& filename) const {
+void ContactData::serialize(const std::string &filename) const {
     std::ofstream file(filename, std::ios::binary | std::ios::app);
     if (!file.is_open()) {
         std::cerr << "Error: Failed to open file for writing." << std::endl;
@@ -91,22 +83,22 @@ void ContactData::serialize(const std::string& filename) const {
     }
 
     const size_t nameLength = name.size();
-    file.write(reinterpret_cast<const char*>(&nameLength), sizeof(nameLength));
+    file.write(reinterpret_cast<const char *>(&nameLength), sizeof(nameLength));
     file.write(name.c_str(), static_cast<std::streamsize>(nameLength));
 
     const size_t lastnameLength = lastname.size();
-    file.write(reinterpret_cast<const char*>(&lastnameLength), sizeof(lastnameLength));
+    file.write(reinterpret_cast<const char *>(&lastnameLength), sizeof(lastnameLength));
     file.write(lastname.c_str(), static_cast<std::streamsize>(lastnameLength));
 
     const size_t phoneNumberLength = phoneNumber.size();
-    file.write(reinterpret_cast<const char*>(&phoneNumberLength), sizeof(phoneNumberLength));
+    file.write(reinterpret_cast<const char *>(&phoneNumberLength), sizeof(phoneNumberLength));
     file.write(phoneNumber.c_str(), static_cast<std::streamsize>(phoneNumberLength));
 
     file.close();
     std::cout << "Object serialized and added to file successfully." << std::endl;
 }
 
-void ContactData::deserialize(const std::string& filename) {
+void ContactData::deserialize(const std::string &filename) {
     std::ifstream file(filename, std::ios::binary);
     if (!file.is_open()) {
         std::cerr << "Error: Failed to open file for reading." << std::endl;
@@ -115,7 +107,7 @@ void ContactData::deserialize(const std::string& filename) {
 
     while (file.peek() != EOF) {
         size_t nameLength;
-        file.read(reinterpret_cast<char*>(&nameLength), sizeof(nameLength));
+        file.read(reinterpret_cast<char *>(&nameLength), sizeof(nameLength));
         const auto nameBuffer = new char[nameLength + 1];
         file.read(nameBuffer, static_cast<std::streamsize>(nameLength));
         nameBuffer[nameLength] = '\0';
@@ -123,7 +115,7 @@ void ContactData::deserialize(const std::string& filename) {
         delete[] nameBuffer;
 
         size_t lastnameLength;
-        file.read(reinterpret_cast<char*>(&lastnameLength), sizeof(lastnameLength));
+        file.read(reinterpret_cast<char *>(&lastnameLength), sizeof(lastnameLength));
         const auto lastnameBuffer = new char[lastnameLength + 1];
         file.read(lastnameBuffer, static_cast<std::streamsize>(lastnameLength));
         lastnameBuffer[lastnameLength] = '\0';
@@ -131,26 +123,24 @@ void ContactData::deserialize(const std::string& filename) {
         delete[] lastnameBuffer;
 
         size_t phonenumberLength;
-        file.read(reinterpret_cast<char*>(&phonenumberLength), sizeof(phonenumberLength));
+        file.read(reinterpret_cast<char *>(&phonenumberLength), sizeof(phonenumberLength));
         const auto phoneBuffer = new char[phonenumberLength + 1];
         file.read(phoneBuffer, static_cast<std::streamsize>(phonenumberLength));
         phoneBuffer[phonenumberLength] = '\0';
         std::string phonenumber(phoneBuffer);
         delete[] phoneBuffer;
 
-        addContactAndSort(ContactData(name, lastname, phonenumber));
+        ContactData(name, lastname, phonenumber);
     }
 
     file.close();
     std::cout << "Objects deserialized successfully." << std::endl;
 }
 
-ContactData::ContactData(const std::string& name, const std::string& lastname, const std::string& phoneNumber)
-{
+ContactData::ContactData(const std::string &name, const std::string &lastname, const std::string &phoneNumber) {
     this->name = name;
     this->lastname = lastname;
     this->phoneNumber = phoneNumber;
 
     addContactAndSort(*this);
-
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,86 @@
+#include <iostream>
 #include <string>
+#include <map>
 #include "../include/ContactData.h"
 
-int main()
-{
-    ContactData con5("adam", "Zakon", "777666999");
-    ContactData con2("Bartek", "Raszka", "669114472");
-    ContactData con1("Adam", "raszka", "669114472");
-    ContactData con4("Damian", "Raszka", "669114472");
-    ContactData con3("Czarek", "Raszka", "669114472");
-    ContactData con7("Adam", "aaszka", "669114472");
+void createContact() {
+    std::string name, lastname, phonenumber;
 
-    ContactData::printList(ContactData::contacts);
+    std::cout << "Please input name:" << std::endl;
+    std::getline(std::cin >> std::ws, name);
+    std::cout << "Please input lastname:" << std::endl;
+    std::getline(std::cin, lastname);
+    std::cout << "Please input phonenumber:" << std::endl;
+    std::getline(std::cin, phonenumber);
 
-    //ContactData::searchContacts("777");
-    //ContactData::removeContact(con3);
+    ContactData(name, lastname, phonenumber).serialize("data.bin");
+}
 
-    ContactData::printList(ContactData::contacts);
-
-    con5.serialize("data.bin");
-    con4.serialize("data.bin");
-    con1.serialize("data.bin");
-
-    //ontactData::deserialize("data.bin");
+void searchInput() {
+    std::string searchParam;
+    std::cout << "Please input what to search for:" << std::endl;
+    std::getline(std::cin >> std::ws, searchParam);
+    ContactData::searchContacts(searchParam);
+}
 
 
+void createMap() {
+    std::map<int, ContactData> mapContacts;
+    int i = 1;
+    int contactToRemove;
 
+    for (auto it = ContactData::contacts.begin(); it != ContactData::contacts.end(); ++it, ++i) {
+        mapContacts.insert({i, **it});
+    }
+    for (const auto &contact: mapContacts) {
+        std::cout << contact.first << ": " << contact.second.name << " " << contact.second.lastname << " " << contact.
+                second.phoneNumber << std::endl;
+    }
+    do {
+        std::cout << "Choose correct number of contact you wish to remove or press 0 if you wish to abandon" <<
+                std::endl;
+        std::cin >> contactToRemove;
+    } while (contactToRemove < 0 || contactToRemove > i);
 
+    if (contactToRemove != 0) {
+        const auto deletedCon = mapContacts.find(contactToRemove)->second;
+        ContactData::removeContact(deletedCon);
+    }
+}
 
-    return 0;
+int main() {
+    bool running = true;
+    uint8_t option;
+    ContactData::deserialize("data.bin");
+
+    while (running) {
+        std::cout << "Please select on of the options listed bellow: " << std::endl << std::endl;;
+        std::cout << "1. List all contacts" << std::endl;
+        std::cout << "2. Add a new contact" << std::endl;
+        std::cout << "3. Delete a contact" << std::endl;
+        std::cout << "4. Search for a contact" << std::endl;
+        std::cout << "5. Exit" << std::endl;
+        std::cin >> option;
+
+        switch (option) {
+            case '1':
+                ContactData::printList(ContactData::contacts);
+                break;
+            case '2':
+                createContact();
+                break;
+            case '3':
+                createMap();
+                break;
+            case '4':
+                searchInput();
+                break;
+            case '5':
+                running = false;
+                break;
+            default:
+                std::cout << "Please input correct value" << std::endl;
+                break;
+        }
+    }
 }
